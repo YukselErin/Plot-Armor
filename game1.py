c10934398f09e0927b91ff8089674552d66c24ef
Yüksel <ykselerin@gmail.com>
2022-04-29 00:11:37 +0300

    added some comments for some functions

--- game1.py
+++ game1.py
@@ -1,6 +1,7 @@
 from random import randint
 import pygame
 from numpy import random
+
 class Game(object):
     enemyTotalHealth = 10
     currentEnemyHealth = 10
@@ -18,7 +19,8 @@ class Game(object):
                 self.coords[0] -=1
             else:
                 self.coords[0] +=1
-    
+
+    """"This object is used to keep track of laser shots that need to be drawn for multiple frames"""
     class Laser(object):
         start, finish, color, duration = (0,0),(0,0),(0,0,0),0
         def __init__(self,start, finish, color, duration ) :
@@ -35,18 +37,21 @@ class Game(object):
         self.health = health
         self.power = power
         self.window = pygame.display.set_mode((self.width,self.height))
-
+    
+    """This function draws a laser from the list and decrements its duration variable, which allows us to control for how many frames a line is drawn."""
     def drawShot(self,laser):
         pygame.draw.line(self.window, laser.color, laser.start, laser.finish)
         laser.duration -= 1
         return laser
 
+    """"This function uses (shots) list to draw all the necessary lines on the screen for each frame."""
     def drawAllShots (self):
         for laser in self.shots:
             laser = self.drawShot(laser)
             if laser.duration == 0:
                 self.shots.remove(laser)
 
+    """This function calculates coordinates for every enemy laser and puts them in a list. Also if the player is hit by the laser the player loses health."""
     def enemyActivity(self):
         for enemy in self.enemies:
             shotX = (enemy + random.randint(300) - 150)
@@ -56,7 +61,7 @@ class Game(object):
             if( 20 > abs(shotX - self.player.coords[0])):
                 self.health -=1
                 
-
+    """"Creates a set number of enemy circles and keeps them in a list."""
     def createEnemy(self):
         gap = (self.width - 20) /5
         self.enemies.append(70)
@@ -67,6 +72,7 @@ class Game(object):
         for enemy in self.enemies:
             pygame.draw.circle(self.window, (255,0,0), (enemy,20), 20)
 
+    """The player shoots its laser at the closest target with perfect accuracy. Also by every shot the enemy loses health. Although there are multiple enemies represented in the visual they all share one health pool."""
     def playerActivity(self):
         min =801
         cord = -1
@@ -108,12 +114,6 @@ class Game(object):
             pygame.display.update()
 
             
-
-            
-            
-           
-
-    
 
 
 game1 = Game(5,10,1)
